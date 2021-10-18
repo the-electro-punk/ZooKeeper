5a043495ce2e1360181a05addb213c6a11383a9f
the-electro-punk <bwr2555@hotmail.com>
2021-10-18 15:25:05 -0500

    added a query filter function

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const app = express()
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log(req.query)

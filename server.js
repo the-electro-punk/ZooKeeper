9bd54b3f691a499211fb95536fe746dc44348cdc
the-electro-punk <bwr2555@hotmail.com>
2021-10-18 15:17:33 -0500

    changed JSON response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
+    res.json(animals);
   });
 
 app.listen(3001, () => {

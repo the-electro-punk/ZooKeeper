44349944c5eee1e93acbc3d306bd6975a9649455
the-electro-punk <bwr2555@hotmail.com>
2021-10-18 15:15:21 -0500

    added code for fetching data

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-// this is fun
+
 const app = express()
+
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+  });
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+  
+// this summons the list of animals
+  const { animals } = require('./data/animals');
